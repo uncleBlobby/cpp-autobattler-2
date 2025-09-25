1971bb625c017a14439cefb937b1e99c0c0e1458
Dustin Christianson <dc.christianson@gmail.com>
2025-09-24 20:26:58 -0600

    sketch out abstract attack idea

---
+++ src/attack.h
@@ -0,0 +1,57 @@
+#ifndef _ATTACK_H
+#define _ATTACK_H
+
+#include "base.h"
+#include "gameobject.h"
+
+class ProjectileAttack {
+  public:
+    int projectileCount;
+    int projectileBaseDamage;
+    int projectileRadius;
+    std::vector<rl::Vector2> projectileDirections;
+    std::vector<rl::Vector2> projectilePositions;
+    float coolDownTime;
+    float velocity;
+    rl::Vector2 startPosition;
+    GameObject &target;
+    rl::Color projectileColor;
+
+    // Standard single projectile targeted attack
+    ProjectileAttack(GameObject &targ, rl::Vector2 startPos) : target(targ) {
+        projectileCount = 1;
+        projectileBaseDamage = 5;
+        projectileRadius = 5;
+        velocity = 200.0f;
+        startPosition = startPos;
+        projectileDirections = std::vector<rl::Vector2>{};
+        coolDownTime = 2.0f;
+        projectileDirections.push_back(
+            GetDirectionToTarget(target.GetPosition(), startPos));
+        projectilePositions.push_back(startPos);
+        projectileColor = rl::BLACK;
+    };
+
+    void Update(float dt) {
+
+        // normalized directions
+
+        std::vector<rl::Vector2> normDirs = {};
+
+        for (size_t i = 0; i < projectileCount; i++) {
+            normDirs.push_back(Vector2Normalize(projectileDirections[i]));
+
+            projectilePositions[i].x += (velocity * normDirs[i].x) * dt;
+            projectilePositions[i].y += (velocity * normDirs[i].y) * dt;
+        }
+    };
+    void Draw() const {
+
+        for (size_t i = 0; i < projectileCount; i++) {
+            rl::DrawCircleV(projectilePositions[i], projectileRadius,
+                            projectileColor);
+        }
+    };
+};
+
+#endif
